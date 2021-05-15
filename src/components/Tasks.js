723c351bf1262d967d614390d49870dfb4251d54
Sreejit <sreejit9821@gmail.com>
2021-05-16 01:04:46 +0530

    Signed-off-by: Sreejit <sreejit9821@gmail.com>

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,8 +1,32 @@
+import { useState } from 'react'
+
+
+
 const Tasks = () => {
+const [tasks, setTasks] = useState([
+{
+ id : 1,
+ text: 'Doctors appointment',
+ day: '15-05-2021 23:45',
+},
+{
+ id : 2,
+ text: 'Doctors appointment 2',
+ day: '15-05-2021 23:45',
+},
+{
+ id : 3,
+ text: 'Doctors appointment 3',
+ day: '15-05-2021 23:45',
+}
+])
+
  return (
-  <div>
-   
-  </div>
+  <>
+   {tasks.map( (task) => {
+   return  <h3 key={task.id}>{task.text}</h3>
+   } )}
+  </>
  )
 }
 
